15ba02e86fca39ecb3a0e8a4c027f36698df2beb
Nguyen Duy Ta <d.nguyen@raksul.com>
2019-05-19 23:23:25 +0700

    add validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_many :microposts
+
+  validates :name, presence: true
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
